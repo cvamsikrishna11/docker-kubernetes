bee82cb5e8a9322bae7daedae659ddb01a127310
vamsi.chunduru <cvamsikrishna11@gmail.com>
2024-03-02 08:56:36 +0530

    added compose userdata

--- docker-user-data.sh
+++ docker-user-data.sh
@@ -4,3 +4,6 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo yum install git -y
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
